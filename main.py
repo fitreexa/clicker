cb0b5a2ef28690ad49d30b232ebbeda488f51e3f
fitreexa <89209562227ib@gmail.com>
2025-05-10 19:05:47 +0300

    добавил зону нажатия

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import pygame
 import sys
 from helper import resource_path
+from sprite import GameSprite
 
 pygame.init()
 
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Кликер")
+background = pygame.image.load(resource_path("assets/main_pole.jpg"))
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 
-background = pygame.image.load(resource_path("assets/main_pole.jpg"))
+asset=resource_path("assets/cube1.jpg")
+button = GameSprite(asset,200, 200, 220, 220)
 
 clicks = 0
 font = pygame.font.SysFont("Arial", 36)
 game = True
 while game:
     screen.blit(background, (0, 0))
+    button.reset(screen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                clicks += 1
+                if button.collidepoint(event.pos):
+                    clicks += 1
     
     text = font.render(f"Кликов: {clicks}", True, BLACK)
     screen.blit(text, (10,10))
---
+++ sprite.py
@@ -0,0 +1,17 @@
+import pygame
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, imaged, x, y, sizex, sizey):
+        super().__init__()
+        self.image = pygame.transform.scale(
+            pygame.image.load(imaged), (sizex, sizey)
+        )
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.sizex = sizex
+        self.sizey = sizey
+    def reset(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+    
+    def collidepoint(self, pos):
+        return self.rect.collidepoint(pos)
