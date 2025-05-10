c0aef4f73f20ba19df14246d33158004dab5f424
fitreexa <89209562227ib@gmail.com>
2025-05-10 22:55:22 +0300

    сделал анимацию и магазин

--- main.py
+++ main.py
@@ -10,18 +10,22 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Кликер")
 background = pygame.image.load(resource_path("assets/main_pole.jpg"))
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
+WHITE = (0, 128, 0)
 
 asset=resource_path("assets/cube1.jpg")
-button = GameSprite(asset,200, 200, 220, 220)
+asset1=resource_path("assets/bul.png")
+button = GameSprite(asset,175, 200, 220, 220)
+dub=GameSprite(asset1,590, 50, 120, 120)
+transparent_surface = pygame.Surface((400, 800), pygame.SRCALPHA) 
+transparent_surface.fill((119, 136, 153, 128))
 
 clicks = 0
-font = pygame.font.SysFont("Arial", 36)
+font = pygame.font.SysFont("Arial", 50)
 game = True
 while game:
     screen.blit(background, (0, 0))
+    screen.blit(transparent_surface, (500, 0))
+    dub.reset(screen)
     button.reset(screen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -30,8 +34,13 @@ while game:
             if event.button == 1:
                 if button.collidepoint(event.pos):
                     clicks += 1
+                    button.rect.y +=10 
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                if button.collidepoint(event.pos):
+                    button.rect.y -= 10
     
-    text = font.render(f"Кликов: {clicks}", True, BLACK)
+    text = font.render(f"Кликов: {clicks}", True, WHITE)
     screen.blit(text, (10,10))
     pygame.display.flip()
 
