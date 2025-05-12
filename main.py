08081bab90e5bbe14fd65be5dce8c76fdcdea9b7
fitreexa <89209562227ib@gmail.com>
2025-05-12 21:46:16 +0300

    добавил магазн предметов

--- main.py
+++ main.py
@@ -14,18 +14,26 @@ WHITE = (0, 128, 0)
 
 asset=resource_path("assets/cube1.jpg")
 asset1=resource_path("assets/bul.png")
+asset2=resource_path("assets/almaz.jpg")
+asset3=resource_path("assets/dube1.jpg")
+asset4=resource_path("assets/sunduk.jpg")
 button = GameSprite(asset,175, 200, 220, 220)
 dub=GameSprite(asset1,590, 50, 120, 120)
+almaz=GameSprite(asset2,590, 250, 120, 120)
+sunduk=GameSprite(asset3,590, 450, 120, 120)
+
 transparent_surface = pygame.Surface((400, 800), pygame.SRCALPHA) 
 transparent_surface.fill((119, 136, 153, 128))
 
 clicks = 0
-font = pygame.font.SysFont("Arial", 50)
+font = pygame.font.SysFont("Arial", 55)
 game = True
 while game:
     screen.blit(background, (0, 0))
     screen.blit(transparent_surface, (500, 0))
     dub.reset(screen)
+    almaz.reset(screen)
+    sunduk.reset(screen)
     button.reset(screen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -40,8 +48,9 @@ while game:
                 if button.collidepoint(event.pos):
                     button.rect.y -= 10
     
-    text = font.render(f"Кликов: {clicks}", True, WHITE)
-    screen.blit(text, (10,10))
+    text = font.render(str(clicks), True, WHITE)
+        # text = font.render(f"Кликов: {clicks}", True, WHITE)
+    screen.blit(text, (20,10))
     pygame.display.flip()
 
 pygame.quit()
